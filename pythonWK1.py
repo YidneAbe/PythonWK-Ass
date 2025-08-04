ee0d88725e9ff036b44a521eeecc07e8ebb05183
YidneAbe <yidabe2016@gmail.com>
2025-08-04 20:05:06 +0300

    Python week-1 Assignment
    Basic calculation

---
+++ pythonWK1.py
@@ -0,0 +1,27 @@
+# Ask the user to enter the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user to enter the second number
+num2 = float(input("Enter the second number: "))
+
+# Ask the user to choose an operation
+operation = input("Choose an operation (+, -, *, /): ")
+
+# Perform the operation
+if operation == '+':
+    result = num1 + num2
+    print("Result:", result)
+elif operation == '-':
+    result = num1 - num2
+    print("Result:", result)
+elif operation == '*':
+    result = num1 * num2
+    print("Result:", result)
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid operation.")
